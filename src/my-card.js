41305ff2c1b67ab011772550adbbd1999988dbbe
ritazheng1011 <ritazheng1011@gmail.com>
2025-02-13 13:20:02 -0500

    commiting for checking

--- src/my-card.js
+++ src/my-card.js
@@ -1,23 +1,23 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 
 /**
  * Now it's your turn. Here's what we need to try and do:
- * 1. Get you HTML from your card working in here 
+ * 1. Get you HTML from your card working in here
  * 2. Get your CSS rescoped as needed to work here
  */
 
 export class MyCard extends LitElement {
-
   static get tag() {
-    return 'my-card';
+    return "my-card";
   }
 
   constructor() {
     super();
-    this.title = "Title"
-    this.description = "Description"
+    this.title = "Title";
+    this.description = "Description";
     this.image = "https://via.placeholder.com/150";
     this.backgroundColor = "pink";
+    this.fancy = false;
   }
 
   static get styles() {
@@ -26,15 +26,22 @@ export class MyCard extends LitElement {
         display: block;
       }
 
+      :host([fancy]) {
+        display: block;
+        background-color: pink;
+        border: 2px solid fuchsia;
+        box-shadow: 10px 5px 5px red;
+      }
+
       .pokemon-card {
-      display: inline-grid;
-      max-width: 800px;
-      border-radius: 100px;
-      margin: 16px;
-      padding: 20px;
-      background-color: pink;
-      text-align: center;
-    }
+        display: inline-grid;
+        max-width: 800px;
+        border-radius: 100px;
+        margin: 16px;
+        padding: 20px;
+        background-color: pink;
+        text-align: center;
+      }
 
       .title {
         color: black;
@@ -58,7 +65,6 @@ export class MyCard extends LitElement {
         margin-bottom: 30px;
       }
 
-
       .btn {
         padding: 16px 65px;
         background-color: purple;
@@ -73,20 +79,37 @@ export class MyCard extends LitElement {
       .btn:hover {
         background-color: lightblue;
       }
-          `;
-        }
+    `;
+  }
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    } else {
+      this.fancy = false;
+    }
+  }
 
   render() {
     return html`
-    <div class="pokemon-card" style="background-color: ${this.backgroundColor};">
-      <h1 class="title">${this.title}</h1>
+      <div
+        class="pokemon-card"
+        style="background-color: ${this.backgroundColor};"
+      >
+        <h1 class="title">${this.title}</h1>
+        <button>Toggle</button>
         <p class="description">${this.description}</p>
-        <img class="image" src="${this.image}" alt="${this.title}"/>
-        <slot></slot>
-      <a href="https://hax.psu.edu">
-        <button class="btn"><em>Details</em></button>
-      </a>
-    </div>
+        <img class="image" src="${this.image}" alt="${this.title}" />
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+          <summary>Description</summary>
+          <div>
+            <slot>${this.description}</slot>
+            <a href="https://hax.psu.edu">
+              <button class="btn"><em>Link for more info</em></button>
+            </a>
+          </div>
+        </details>
+      </div>
     `;
   }
 
@@ -98,7 +121,7 @@ export class MyCard extends LitElement {
       image: { type: String },
       backgroundColor: { type: String },
     };
-  }  
+  }
 }
 
 globalThis.customElements.define(MyCard.tag, MyCard);
